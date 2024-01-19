60ed688e22eb191440434e22d1bb6c55da7378aa
Patrick Navega <psnavega97@icloud.com>
2024-01-18 23:31:38 -0300

    feat: create initial setup

---
+++ ApiContacts/Controllers/ContatoController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ApiContacts.Controllers
+{
+    public class ContatoController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        public IActionResult Editar()
+        {
+            return View();
+        }
+
+        public IActionResult ApagarConfirmacao()
+        {
+            return View();
+        }
+    }
+}
+
---
+++ ApiContacts/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiContacts.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
